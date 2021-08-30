27b95dfb889a6d89fc5bf6f141b60a600ea7f1cd
gilesnknight <62376886+gilesnknight@users.noreply.github.com>
2021-08-30 15:33:03 +0800

    multi group carbon table demo

--- scripts/summaryTables.R
+++ scripts/summaryTables.R
@@ -1,39 +1,9 @@
-library(readxl)
-library(kableExtra)
-library(rmarkdown)
-carbon <- read_excel('tables/carbonTable.xlsx', sheet = 3)
-carbonGroups <- unique(carbon$Group)
-carbon$`AED name` <- paste0("`",carbon$`AED name`,"`")
-
-for(i in seq_along(carbon$Symbol)){
-  if(!is.na(carbon$Symbol[i])==TRUE){
-    carbon$Symbol[i] <- paste0("$$",carbon$Symbol[i],"$$")
-  } else {
-    carbon$Symbol[i] <- NA
-  }
-}
-
-kbl(carbon[,2:8], caption = "Carbon table") %>%
-  kable_paper("striped", full_width = F) %>%
-  pack_rows(carbonGroups[1], 
-            min(which(carbon$Group == carbonGroups[1])), 
-            max(which(carbon$Group == carbonGroups[1]))) %>% 
-  pack_rows(carbonGroups[2], 
-            min(which(carbon$Group == carbonGroups[2])), 
-            max(which(carbon$Group == carbonGroups[2]))) %>% 
-  pack_rows(carbonGroups[3], 
-            min(which(carbon$Group == carbonGroups[3])), 
-            max(which(carbon$Group == carbonGroups[3]))) %>% 
-  row_spec(0, background = "#14759e", bold = TRUE, color = "white") %>% 
-  kable_styling(full_width = F)
-
-
-
 library(knitr)
 library(kableExtra)
 library(readxl)
 library(rmarkdown)
-carbon <- read_excel('tables/carbonTable.xlsx', sheet = 3)
+carbon <- read_excel('tables/carbonTableNewGrouping.xlsx', sheet = 3)
+carbon <- carbon[carbon$Table == "Diagnostics",]
 carbonGroups <- unique(carbon$Group)
 carbon$`AED name` <- paste0("`",carbon$`AED name`,"`")
 
@@ -45,20 +15,16 @@ for(i in seq_along(carbon$Symbol)){
   }
 }
 
-kbl(carbon[,2:8], caption = "Carbon table", align = "l",) %>%
-  pack_rows(carbonGroups[1], 
-            min(which(carbon$Group == carbonGroups[1])), 
+kbl(carbon[,3:NCOL(carbon)], caption = "Carbon - diagnostics", align = "l",) %>%
+  pack_rows(carbonGroups[1],
+            min(which(carbon$Group == carbonGroups[1])),
             max(which(carbon$Group == carbonGroups[1])),
-            background = '#ebebeb') %>% 
-  pack_rows(carbonGroups[2], 
-            min(which(carbon$Group == carbonGroups[2])), 
+            background = '#ebebeb') %>%
+  pack_rows(carbonGroups[2],
+            min(which(carbon$Group == carbonGroups[2])),
             max(which(carbon$Group == carbonGroups[2])),
-            background = '#ebebeb') %>% 
-  pack_rows(carbonGroups[3], 
-            min(which(carbon$Group == carbonGroups[3])), 
-            max(which(carbon$Group == carbonGroups[3])),
-            background = '#ebebeb') %>% 
-  row_spec(0, background = "#14759e", bold = TRUE, color = "white") %>% 
-  kable_styling(full_width = F,font_size = 12) %>% 
+            background = '#ebebeb') %>%
+  row_spec(0, background = "#14759e", bold = TRUE, color = "white") %>%
+  kable_styling(full_width = F,font_size = 12) %>%
   scroll_box(width = "770px", height = "500px",
-             fixed_thead = FALSE) 
+             fixed_thead = FALSE)
